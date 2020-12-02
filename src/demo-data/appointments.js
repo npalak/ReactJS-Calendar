1a62a4314a183d63d81ab693d7e21ea6855b700e
neha <abc@gmial.com>
2020-12-02 23:05:43 +0530

    first commit

---
+++ src/demo-data/appointments.js
@@ -0,0 +1,17 @@
+export const appointments = [
+    {
+      title: "Website Re-Design Plan",
+      startDate: new Date(2020, 5, 25, 9, 35),
+      endDate: new Date(2020, 5, 25, 11, 30),
+      id: 0,
+      location: "Room 1"
+    },
+    {
+      title: "Book Flights to San Fran for Sales Trip",
+      startDate: new Date(2020, 5, 25, 12, 11),
+      endDate: new Date(2020, 5, 25, 13, 0),
+      id: 1,
+      location: "Room 1"
+    }
+  ];
+  
